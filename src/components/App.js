75e116848ec69dcfefa1518409ce3a9a5496543a
lavanya94lava <singhlavanya94@gmail.com>
2020-05-25 19:25:10 +0530

    add Navbar and Router functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import PropTypes from "prop-types";
+
 import { fetchPosts } from "../actions/posts";
-import { PostsList } from "./";
+import { PostsList, Navbar } from "./";
+
+const Login = () => (<div> Login </div>);
+
+const SignUp = () => <div>SignUp</div>
+
+const Home = () => <div>Home</div>
 
 class App extends React.Component {
   componentDidMount() {
@@ -9,9 +18,29 @@ class App extends React.Component {
   }
 
   render() {
-    console.log("props", this.props);
     const { posts } = this.props;
-    return <PostsList posts={posts}/>;
+    return (
+      <Router>
+        <div>
+          <Navbar />
+          {/* <PostsList posts={posts} /> */}
+          <ul>
+            <li>
+              <Link to ="/"> Home </Link>
+            </li>
+            <li>
+              <Link to ="/login"> Login </Link>
+            </li>
+            <li>
+              <Link to ="/signup"> SignUp </Link>
+            </li>
+          </ul>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
+        </div>
+      </Router>
+    );
   }
 }
 
@@ -20,5 +49,9 @@ function mapStateToProps(state) {
     posts: state.posts,
   };
 }
+
+App.propTypes = {
+  posts: PropTypes.array.isRequired,
+};
 
 export default connect(mapStateToProps)(App);
